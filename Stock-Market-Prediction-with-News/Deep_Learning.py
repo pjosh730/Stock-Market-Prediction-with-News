fdd3cbb3270ab75b0aedd64cba7bfd80b0210a3f
Weikun Hu <weikunhu0428@gmail.com>
2019-12-07 15:28:01 -0800

    add model

---
+++ Stock-Market-Prediction-with-News/Deep_Learning.py
@@ -0,0 +1,362 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import re
+import nltk
+import pandas as pd
+import numpy as np
+
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+english_stemmer=nltk.stem.SnowballStemmer('english')
+
+from sklearn.feature_selection.univariate_selection import SelectKBest, chi2, f_classif
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
+from sklearn.linear_model import SGDClassifier, SGDRegressor,LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import random
+import itertools
+
+import sys
+import os
+import argparse
+from sklearn.pipeline import Pipeline
+from scipy.sparse import csr_matrix
+from sklearn.feature_extraction.text import CountVectorizer
+import six
+from abc import ABCMeta
+from scipy import sparse
+from scipy.sparse import issparse
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils import check_X_y, check_array
+from sklearn.utils.extmath import safe_sparse_dot
+from sklearn.preprocessing import normalize, binarize, LabelBinarizer
+from sklearn.svm import LinearSVC
+
+from keras.models import model_from_json
+from keras.preprocessing import sequence
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Lambda
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM, SimpleRNN, GRU
+from keras.preprocessing.text import Tokenizer
+from collections import defaultdict
+from keras.layers.convolutional import Convolution1D
+from keras import backend as K
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import cm
+get_ipython().run_line_magic('matplotlib', 'inline')
+plt.style.use('ggplot')
+
+
+# In[18]:
+
+
+
+from keras.preprocessing import sequence
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Lambda
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM, SimpleRNN, GRU
+from keras.preprocessing.text import Tokenizer
+from collections import defaultdict
+from keras.layers.convolutional import Convolution1D
+from keras import backend as K
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import pickle
+
+
+# In[3]:
+
+
+data = pd.read_csv('../Data/Combined_News_DJIA.csv')
+train = data[data['Date'] < '2015-01-01']
+test = data[data['Date'] > '2014-12-31']
+
+
+# ## Text cleaning (this code didn't do much, I will add something this week)
+
+# ### Training set
+
+# In[4]:
+
+
+trainheadlines = []
+for row in range(0,len(train.index)):
+    trainheadlines.append(' '.join(str(x) for x in train.iloc[row,2:27]))
+
+
+# ### Test set
+
+# In[5]:
+
+
+testheadlines = []
+for row in range(0,len(test.index)):
+    testheadlines.append(' '.join(str(x) for x in test.iloc[row,2:27]))
+
+
+# ### Deep Learning
+
+# In[16]:
+
+
+testheadlines
+
+
+# In[ ]:
+
+
+advancedtrain
+
+
+# In[20]:
+
+
+batch_size = 32
+nb_classes = 2
+advancedvectorizer = TfidfVectorizer( min_df=0.04, max_df=0.3, max_features = 200000, ngram_range = (2, 2))
+advancedtrain = advancedvectorizer.fit(trainheadlines)
+pickle.dump(advancedtrain, open("../Model/tfidf1.pkl", "wb"))
+
+
+# In[6]:
+
+
+batch_size = 32
+nb_classes = 2
+advancedvectorizer = TfidfVectorizer( min_df=0.04, max_df=0.3, max_features = 200000, ngram_range = (2, 2))
+advancedtrain = advancedvectorizer.fit_transform(trainheadlines)
+testheadlines = []
+for row in range(0,len(test.index)):
+    testheadlines.append(' '.join(str(x) for x in test.iloc[row,2:27]))
+advancedtest = advancedvectorizer.transform(testheadlines)
+print(advancedtrain.shape)
+
+X_train = advancedtrain.toarray()
+X_test = advancedtest.toarray()
+
+print('X_train shape:', X_train.shape)
+print('X_test shape:', X_test.shape)
+y_train = np.array(train["Label"])
+y_test = np.array(test["Label"])
+
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+
+# pre-processing: divide by max and substract mean
+scale = np.max(X_train)
+X_train /= scale
+X_test /= scale
+
+mean = np.mean(X_train)
+X_train -= mean
+X_test -= mean
+
+input_dim = X_train.shape[1]
+
+# Here's a Deep Dumb MLP (DDMLP)
+model = Sequential()
+model.add(Dense(256, input_dim=input_dim))
+model.add(Activation('relu'))
+model.add(Dropout(0.4))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.4))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+# we'll use categorical xent for the loss, and RMSprop as the optimizer
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+print("Training...")
+#model.fit(X_train, Y_train, nb_epoch=2, batch_size=16, validation_split=0.15, show_accuracy=True)
+model.fit(X_train, Y_train, nb_epoch=2, batch_size=16, validation_split=0.15)
+
+print("Generating test predictions...")
+preds14 = model.predict_classes(X_test, verbose=0)
+acc14 = accuracy_score(test["Label"], preds14)
+
+print('prediction accuracy: ', acc14)
+
+
+# In[7]:
+
+
+### LSTM
+
+max_features = 10000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.1
+maxlen = 200
+batch_size = 32
+nb_classes = 2
+
+# vectorize the text samples into a 2D integer tensor
+tokenizer = Tokenizer(nb_words=max_features)
+tokenizer.fit_on_texts(trainheadlines)
+sequences_train = tokenizer.texts_to_sequences(trainheadlines)
+sequences_test = tokenizer.texts_to_sequences(testheadlines)
+
+print('Pad sequences (samples x time)')
+X_train = sequence.pad_sequences(sequences_train, maxlen=maxlen)
+X_test = sequence.pad_sequences(sequences_test, maxlen=maxlen)
+
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+
+print('X_train shape:', X_train.shape)
+print('X_test shape:', X_test.shape)
+
+print('Build model...')
+model = Sequential()
+model.add(Embedding(max_features, 128, dropout=0.2))
+model.add(LSTM(128, dropout_W=0.2, dropout_U=0.2)) 
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+print('Train...')
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=3,
+          validation_data=(X_test, Y_test))
+score, acc = model.evaluate(X_test, Y_test,
+                            batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+
+print("Generating test predictions...")
+preds15 = model.predict_classes(X_test, verbose=0)
+acc15 = accuracy_score(test['Label'], preds15)
+
+print('prediction accuracy: ', acc15)
+
+
+# In[13]:
+
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("../Model/model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("../Model/model.h5")
+print("Saved model to disk")
+ 
+# later...
+ 
+
+
+# In[ ]:
+
+
+
+
+
+# In[14]:
+
+
+# load json and create model
+json_file = open('../Model/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("../Model/model.h5")
+print("Loaded model from disk")
+ 
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+# score = loaded_model.evaluate(X, Y, verbose=0)
+# print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
+
+# In[8]:
+
+
+## CNN
+
+nb_filter = 120
+filter_length = 2
+hidden_dims = 120
+nb_epoch = 2
+
+print('Build model...')
+model = Sequential()
+model.add(Embedding(max_features, 128, dropout=0.2))
+# we add a Convolution1D, which will learn nb_filter
+# word group filters of size filter_length:
+model.add(Convolution1D(nb_filter=nb_filter,
+                        filter_length=filter_length,
+                        border_mode='valid',
+                        activation='relu',
+                        subsample_length=1))
+
+def max_1d(X):
+    return K.max(X, axis=1)
+
+model.add(Lambda(max_1d, output_shape=(nb_filter,)))
+model.add(Dense(hidden_dims)) 
+model.add(Dropout(0.2)) 
+model.add(Activation('relu'))
+model.add(Dense(nb_classes))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+print('Train...')
+model.fit(X_train, Y_train, batch_size=32, nb_epoch=1,
+          validation_data=(X_test, Y_test))
+score, acc = model.evaluate(X_test, Y_test,
+                            batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+
+print("Generating test predictions...")
+preds16 = model.predict_classes(X_test, verbose=0)
+acc16 = accuracy_score(test['Label'], preds16)
+
+print('prediction accuracy: ', acc16)
+
+
+
+
+
+
+# In[15]:
+
+
+X_test
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
